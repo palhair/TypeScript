223824246a90ba2f180636bdccd34d081545af6e
palhair <palad.ru@mail.ru>
2022-10-03 14:52:16 +0400

    упражнение типизируем функц

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 "use strict";
-// readonly
-const skills = [1, 'Dev'];
-const skill = ['dev', "dev"];
-//enum
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var StatusCode;
 (function (StatusCode) {
-    StatusCode["SUCCESS"] = "s";
-    StatusCode["IN_PROCESS"] = "p";
-    StatusCode["FAILED"] = "f";
+    StatusCode["PUBL"] = "published";
+    StatusCode["DRAFT"] = "draft";
+    StatusCode["DEL"] = "deleted";
 })(StatusCode || (StatusCode = {}));
-const res = {
-    message: 'Платеж успешен',
-    statusCode: StatusCode.SUCCESS
-};
-if (res.statusCode === StatusCode.SUCCESS) {
+function getFaqs(req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield fetch('/faqs', {
+            method: 'POST',
+            body: JSON.stringify(req)
+        });
+        const data = yield res.json();
+        return data;
+    });
 }
-function action(status) {
-}
-action(StatusCode.IN_PROCESS);
-const res2 = 1 /* Roles.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,41 +1,25 @@
-
-// readonly
-
-const skills: readonly [number, string]  = [1, 'Dev'];
-
-const skill: readonly string[] = ['dev', "dev"];
-
-//enum
 enum StatusCode {
-    SUCCESS = 's',
-    IN_PROCESS = 'p',
-    FAILED = 'f'
+    PUBL = 'published',
+    DRAFT = 'draft',
+    DEL = 'deleted'
 }
 
-const res = {
-    message: 'Платеж успешен',
-    statusCode: StatusCode.SUCCESS
+async function getFaqs(req:{
+    topicId: number;
+    status: StatusCode;
+}): Promise<{
+    question: string;
+    answer: string;
+    tags: string[];
+    likes: number;
+    status: StatusCode;
+    
+    }[]> {
+    const res = await fetch('/faqs', {
+        method: 'POST',
+        body: JSON.stringify(req)
+    });
+    const data = await res.json();
+    return data;
+    
 }
-
-if (res.statusCode === StatusCode.SUCCESS){
-
-}
-
-function action (status: StatusCode){
-
-}
-
-action(StatusCode.IN_PROCESS)
-
-
-//1 - успех
-//2 - в процессе
-//3 - отклонен
-
-const enum Roles {
-    ADMIN = 1,
-    USER = 2
-}
-
-
-const res2 = Roles.ADMIN;
