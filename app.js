352bf5ba5caf917bcc6e59c291ed2b2649b4eaaf
palhair <palad.ru@mail.ru>
2022-10-03 13:28:51 +0400

    tuples

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
 "use strict";
-//типизация функций
-function getFullName(userEntity) {
-    return `${userEntity.firstName} ${userEntity.surName}`;
-}
-const getFullNameArrow = (firstName, surName) => {
-    return `${firstName} ${surName}`;
-};
-const user = {
-    firstName: 'Ralf',
-    surName: 'Ringer',
-    city: 'London',
-    age: 97,
-    skills: {
-        dev: true,
-        devops: true
-    }
-};
-console.log(getFullName(user));
-//типизация объекта
-let info = {
-    "officeId": 45,
-    "isOpened": false,
-    "contacts": {
-        "phone": "+79100000000",
-        "email": "my@email.ru",
-        "address": {
-            "city": "Москва"
-        }
-    }
-};
-// типизация массива
-const skills = ['Dev', "DevOps", 'Testing'];
-for (const skill of skills) {
-    console.log(skill.toLocaleLowerCase());
-}
-const res = skills
-    .filter((s) => s !== 'DevOps')
-    .map(s => s + '! ')
-    .reduce((a, b) => a + b);
-console.log(res);
+// tuples
+const skills = [1, 'Dev'];
+const id = skills[0];
+const skillName = skills[1];
+skills.push('dfdfdf'); //push не ограничен
+//const nowork = skills[2];
+skills.pop();
+const [idd, skill] = skills;
+const arr = [1, 'ffee', false, true];
--- app.ts
+++ app.ts
@@ -1,64 +1,14 @@
 
-//типизация функций
-function getFullName(userEntity: {firstName: string, surName:string}): string{
-    return `${userEntity.firstName} ${userEntity.surName}`;
-}
+// tuples
 
-const getFullNameArrow = (firstName: string, surName: string): string => {
-    return `${firstName} ${surName}`;
-}
+const skills: [number, string]  = [1, 'Dev'];
+const id = skills[0];
+const skillName = skills[1];
 
- 
+skills.push('dfdfdf'); //push не ограничен
+//const nowork = skills[2];
+skills.pop();
 
+const [idd, skill] = skills;
 
-const user = {
-    firstName: 'Ralf',
-    surName: 'Ringer', 
-    city: 'London', 
-    age: 97,
-    skills: {
-        dev: true,
-        devops: true
-    }
-}
-
-console.log(getFullName(user));
-//типизация объекта
-let info: {
-    officeId: number;
-    isOpened: boolean;
-    contacts :{
-        phone: string;
-        email: string;
-        address: {
-            city: string;
-        }
-    }
-} = {
-	"officeId": 45,
-	"isOpened": false,
-	"contacts": {
-		"phone": "+79100000000",
-		"email": "my@email.ru",
-		"address": {
-			"city": "Москва"
-		}
-	}
-}
-
-// типизация массива
-
-const skills: string[]  = ['Dev', "DevOps", 'Testing'];
-
-for (const skill of skills) {
-    console.log(skill.toLocaleLowerCase());
-}
-
-
-const res = skills
-.filter((s: string) => s !== 'DevOps')
-.map(s => s + '! ')
-.reduce((a, b) => a + b);
-console.log(res);
-
-
+const arr: [number, string, ...boolean[]] = [1, 'ffee', false, true];
