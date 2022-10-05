ac90f2d97b3115405cf5628c135cc3b2fa5c7fc8
palhair <palad.ru@mail.ru>
2022-10-05 13:34:13 +0400

    тип never

--- app.js
+++ app.js
@@ -1,57 +1,23 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-function logId(id) {
-    console.log(id);
+function genereateError(message) {
+    throw new Error(message);
 }
-const a = logId(1);
-function multiply(a, b) {
-    if (!a) {
-        return a * a;
-    }
+function dumpError() {
+    while (true) { }
 }
-const f1 = () => {
-};
-const f2 = () => {
-    return true;
-};
-const b = f2();
-const skills = ['Dev', 'DevOps'];
-const user = {
-    s: ['s']
-};
-skills.forEach((skill) => user.s.push(skill));
-//unknown
-let input;
-input = 3;
-input = {};
-let res = input;
-let res1 = res;
-function run(i) {
-    if (typeof i == 'number') {
-        i++;
-    }
-    else {
-        i;
-    }
+function rec() {
+    return rec();
 }
-run(input);
-function getData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield fetch('');
-        }
-        catch (e) {
-            if (e instanceof Error) {
-                console.log(e.message);
-            }
-        }
-    });
+function processAction(action) {
+    switch (action) {
+        case 'refund':
+            //...
+            break;
+        case 'checkout':
+            //...
+            break;
+        default:
+            const _ = action;
+            throw new Error('Нет такого action');
+    }
 }
--- app.ts
+++ app.ts
@@ -1,65 +1,37 @@
-function logId(id: string | number ): void {
-    console.log(id);
-}
-const a = logId(1);
-
-function multiply(a: number, b?: number){
-    if (!a){
-        return a * a;
-    }
+function genereateError(message: string): never {
+    throw new Error(message);
 }
 
-type voidFunction = () => void;
-
-const f1 : voidFunction =() =>{
-
-}
-
-const f2 : voidFunction =() =>{
-    return true;
-}
-
-const b = f2();
-
-const skills =['Dev', 'DevOps'];
-
-const user = {
-    s: ['s']
-}
-
-skills.forEach((skill) => user.s.push(skill));
-
-//unknown
-
-let input: unknown;
-
-input = 3;
-input = {};
-
-let res: any = input;
-let res1: string = res;
-
-function run (i: unknown){
-    if(typeof i == 'number'){
-        i++;
-    } else {
-        i;
-    }
+function dumpError(): never {
     
-} 
-
-run(input);
-
-async function getData() {
-    try {
-        await fetch('')
-    } catch(e) {
-        if (e instanceof Error){
-        console.log(e.message);
-        }
-    }
+    while(true) {}
 }
 
-type U1 =  unknown | string;
+function rec(): never {    
+    return rec();
+}
 
-type I1 = unknown & string;
+type paymentAction = 'refund' | 'checkout' | 'reject';
+
+function processAction(action: paymentAction){
+    switch (action){
+        case 'refund':
+            //...
+            break;
+        case 'checkout':
+            //...
+            break;
+        default:
+            const _: never = action;
+            throw new Error('Нет такого action')
+    }
+}
+//never помогает блокировать векту условий в которую мы никогда не должны попасть
+function isString(x: string | number): boolean {
+    if (typeof x === 'string'){
+        return true;
+    }else if (typeof x === 'number'){
+        return false;
+    } 
+    genereateError('dfsd');
+}
