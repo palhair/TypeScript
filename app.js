d8ec30c638e7168780cc81694d76a712f9ae5b9d
palhair <palad.ru@mail.ru>
2022-10-05 14:49:32 +0400

    Приведение типов ts

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 "use strict";
-function genereateError(message) {
-    throw new Error(message);
-}
-function dumpError() {
-    while (true) { }
-}
-function rec() {
-    return rec();
-}
-function processAction(action) {
-    switch (action) {
-        case 'refund':
-            //...
-            break;
-        case 'checkout':
-            //...
-            break;
-        default:
-            const _ = action;
-            throw new Error('Нет такого action');
+const n = null;
+const n1 = null;
+function getUser() {
+    if (Math.random() > 0.5) {
+        return null;
     }
+    else {
+        return {
+            name: "Вася"
+        };
+    }
+}
+const user = getUser();
+if (user) {
+    const n55 = user.name;
+}
+let a = 5;
+let b = a.toString();
+let e = new String(a).valueOf();
+let c = 'cdsf';
+let d = parseInt(c);
+const user1 = {
+    name: "Vasya",
+    email: 'fsdf@fjk.ru',
+    login: 'vasia'
+};
+const user2 = {
+    name: "Vasya",
+    email: 'fsdf@fjk.ru',
+    login: 'vasia'
+};
+const user3 = {
+    name: "Vasya",
+    email: 'fsdf@fjk.ru',
+    login: 'vasia'
+};
+const admin = Object.assign(Object.assign({}, user1), { role: 1 });
+function userToAdmin(user) {
+    return {
+        name: user.name,
+        role: 1
+    };
 }
--- app.ts
+++ app.ts
@@ -1,37 +1,70 @@
-function genereateError(message: string): never {
-    throw new Error(message);
+const n: null = null;
+const n1: any = null;
+
+
+interface User {
+    name: string;
 }
 
-function dumpError(): never {
-    
-    while(true) {}
-}
-
-function rec(): never {    
-    return rec();
-}
-
-type paymentAction = 'refund' | 'checkout' | 'reject';
-
-function processAction(action: paymentAction){
-    switch (action){
-        case 'refund':
-            //...
-            break;
-        case 'checkout':
-            //...
-            break;
-        default:
-            const _: never = action;
-            throw new Error('Нет такого action')
+function getUser() {
+    if (Math.random() > 0.5){
+        return null;
+    }else{
+        return {
+            name: "Вася"
+        } as User
     }
 }
-//never помогает блокировать векту условий в которую мы никогда не должны попасть
-function isString(x: string | number): boolean {
-    if (typeof x === 'string'){
-        return true;
-    }else if (typeof x === 'number'){
-        return false;
-    } 
-    genereateError('dfsd');
+
+const user = getUser();
+if(user){
+    const n55 = user.name;
+}
+
+let a = 5;
+let b: string = a.toString();
+let e: string = new String(a).valueOf();
+
+let c = 'cdsf';
+let d: number = parseInt(c);
+
+interface User {
+    name: string;
+    login: string;
+    email: string;
+}
+
+const user1: User = {
+    name: "Vasya",
+    email: 'fsdf@fjk.ru',
+    login: 'vasia'
+}
+
+const user2 = {
+    name: "Vasya",
+    email: 'fsdf@fjk.ru',
+    login: 'vasia'
+} as User
+
+const user3: User = <User> {//не валидна в react
+    name: "Vasya",
+    email: 'fsdf@fjk.ru',
+    login: 'vasia'
+}
+
+interface Admin {
+    name: string;
+    role: number;
+}
+
+const admin: Admin = {//чем так
+    ...user1,
+    role: 1
+}
+
+function userToAdmin(user: User): Admin{// лучше делать так
+    return {
+        name: user.name,
+        role: 1
+    }
 }
