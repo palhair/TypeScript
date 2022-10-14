7e0c3846e54898674d86368480f049661a2db5d9
palhair <palad.ru@mail.ru>
2022-10-14 14:52:11 +0400

    функции с generic

--- build/app.js
+++ build/app.js
@@ -1,22 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-const a = [1, 2, 3];
-function test() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const b = yield new Promise((resolve, reject) => {
-            resolve(1);
-        });
-    });
+function logMiddleware(data) {
+    console.log(data);
+    return data;
 }
-const check = {
-    drive: true,
-    kpp: false
-};
+const res = logMiddleware([12, 33]);
+function getHalfArr(data) {
+    const l = data.length / 2;
+    return data.splice(0, l);
+}
+getHalfArr([1, 4, 22]);
+getHalfArr([1, 4, "22"]);
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-const a: Array<number> = [1, 2, 3];
-async function test() {
-    const b = await new Promise<number>((resolve, reject) => {
-        resolve(1);
-    })
+function logMiddleware<T>(data: T): T {
+    console.log(data);
+    return data;
 }
 
-const check: Record<string, boolean> ={
-    drive: true,
-    kpp: false
+const res = logMiddleware<number []>([12, 33]);
+
+function getHalfArr<T>(data: Array<T>): Array<T> {
+    const l = data.length/2;
+    return data.splice(0, l);
 }
+
+getHalfArr<number>([1, 4, 22]);
+getHalfArr([1, 4, "22"]);
