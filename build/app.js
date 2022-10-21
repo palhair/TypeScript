611130412ff3861bc8e7785a58d842045beb34ca
palhair <palad.ru@mail.ru>
2022-10-21 12:56:30 +0400

    infer

--- build/app.js
+++ build/app.js
@@ -1,33 +1,8 @@
 "use strict";
-const a = Math.random() > 0.5 ? 1 : 0;
-const success = {
-    code: 200,
-    data: 'done'
+function runTransaction(transaction) {
+    console.log(transaction);
+}
+const transaction = {
+    fromTo: ['1', '2']
 };
-const err = {
-    code: 200,
-    data: new Error()
-};
-//Example
-class User {
-}
-class UserPersistend extends User {
-}
-function getUser(IdOrdbId) {
-    if (typeof IdOrdbId === 'string') {
-        return new UserPersistend();
-    }
-    else {
-        return new User;
-    }
-}
-function getUser2(id) {
-    if (typeof id === 'number') {
-        return new User();
-    }
-    else {
-        return new UserPersistend();
-    }
-}
-const res = getUser2(1);
-const res1 = getUser2('dfsdf1');
+runTransaction(transaction);
--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,14 @@
-const a: number = Math.random() > 0.5 ? 1 : 0;
-
-interface HTTPResponse <T extends 'success' | "failed"> {
-    code: number;
-    data: T extends 'success'? string: Error;
-    
-    
-
+function runTransaction(transaction: {
+   fromTo: [string, string] 
+}) {
+    console.log(transaction);
 }
 
-const success: HTTPResponse <'success'> = {
-    code:200,
-    data: 'done'
+const transaction: GetFirstArg <typeof runTransaction> = {
+    fromTo: ['1', '2']
 }
 
-const err: HTTPResponse <'failed'> = {
-    code:200,
-    data: new Error()
-}
 
-//Example
-class User {
-    id: number;
-    name: string;
-}
 
-class UserPersistend extends User{
-    dbId: string;
-}
-
-function getUser(dbId: string ): UserPersistend
-function getUser(id: number): User 
-function getUser(IdOrdbId: string | number): User | UserPersistend {
-    if(typeof IdOrdbId === 'string'){
-        return new UserPersistend();
-    }else{
-        return new User;
-    }
-}
-
-type UserOrUserPersistend <T extends string | number> = T extends number ? User : UserPersistend;
-
-function getUser2<T extends string | number>(id: T): UserOrUserPersistend<T> {
-    if(typeof id === 'number'){
-        return new User() as UserOrUserPersistend<T>;
-    }else{
-        return new UserPersistend() as UserOrUserPersistend<T>;
-    }
-}
-
-const res = getUser2(1);
-const res1 = getUser2('dfsdf1')
+type GetFirstArg <T> = T extends (first: infer First, ...args: any[]) => any ? First : never;
+runTransaction (transaction);
