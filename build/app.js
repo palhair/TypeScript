4488b95702be23123951f6cded537d187bcc5a99
palhair <palad.ru@mail.ru>
2022-10-21 15:28:29 +0400

    tamplate literal types

--- build/app.js
+++ build/app.js
@@ -1,9 +1,4 @@
 "use strict";
-const from = {
-    name: 'Lord',
-    password: '123'
-};
-const fromValidation = {
-    name: { isValid: true },
-    password: { isValid: false, errorMessage: 'Должен быть длиннее 5 символов' },
+const a = {
+    result: 'httpSuccess'
 };
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,18 @@
+type ReadOrWrite = 'canread' | 'canwrite';
+type Bulk = 'bulk' | '';
 
+type Access = `can${Capitalize<ReadOrWrite>}${Capitalize<Bulk>}`;
 
-interface IForm {
-    name: string;
-    password: string;
-    
+type ReadOrWriteBulk<T> = T extends `can${infer R}`? R: never;
+
+type T = ReadOrWriteBulk<ReadOrWrite>;
+
+type ErrorOrSuccess = 'error' | 'success';
+
+type ResponseT = {
+    result: `http${Capitalize<ErrorOrSuccess>}`
 }
 
-const from: IForm = {
-    name: 'Lord',
-    password: '123'
-    
+const a: ResponseT = {
+    result: 'httpSuccess'
 }
-
-const fromValidation: ValidationType = {
-    name: {isValid: true},
-    password: {isValid: false, errorMessage: 'Должен быть длиннее 5 символов'},
-    
-}
-
-type Validation<Type> = {
-    [Property in keyof Type]-?:  {
-        isValid: true
-    } | {
-        isValid: false;
-        errorMessage: string;
-    }
-} 
-
-
-type ValidationType = Validation<IForm>;
