f335cb758e8c99fd8c84654e8f240e8246fa68b0
palhair <palad.ru@mail.ru>
2022-10-21 11:14:42 +0400

    072 Conditional Types

--- build/app.js
+++ build/app.js
@@ -1,13 +1,33 @@
 "use strict";
-const user = {
-    name: 'waer',
-    roles: [],
-    permission: {
-        endDate: new Date
-    }
+const a = Math.random() > 0.5 ? 1 : 0;
+const success = {
+    code: 200,
+    data: 'done'
 };
-const nameUser = user['name'];
-const roleNames = 'roles';
-let roleNames2 = 'roles';
-const roles = ['admin', 'user', 'super-user'];
-const dfdf = 'user';
+const err = {
+    code: 200,
+    data: new Error()
+};
+//Example
+class User {
+}
+class UserPersistend extends User {
+}
+function getUser(IdOrdbId) {
+    if (typeof IdOrdbId === 'string') {
+        return new UserPersistend();
+    }
+    else {
+        return new User;
+    }
+}
+function getUser2(id) {
+    if (typeof id === 'number') {
+        return new User();
+    }
+    else {
+        return new UserPersistend();
+    }
+}
+const res = getUser2(1);
+const res1 = getUser2('dfsdf1');
--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,52 @@
-interface Role {
-    name: string 
-}
+const a: number = Math.random() > 0.5 ? 1 : 0;
 
-interface Permission {
-    endDate: Date;
-}
-interface User {
-    name: string;
-    roles: Role[];
-    permission: Permission;
-}
-
-const user: User = {
-    name: 'waer',
-    roles:[],
-    permission: {
-        endDate: new Date
-    }
+interface HTTPResponse <T extends 'success' | "failed"> {
+    code: number;
+    data: T extends 'success'? string: Error;
     
+    
+
 }
 
-const nameUser = user['name'];
-const roleNames = 'roles';
-let roleNames2: 'roles' = 'roles';
+const success: HTTPResponse <'success'> = {
+    code:200,
+    data: 'done'
+}
 
-type rolesType = User['roles'];
-type rolesType2 = User[typeof roleNames];
-type rolesType3 = User[typeof roleNames2];
+const err: HTTPResponse <'failed'> = {
+    code:200,
+    data: new Error()
+}
 
+//Example
+class User {
+    id: number;
+    name: string;
+}
 
-type roleType = User['roles'][number];
-type dateType = User['permission']['endDate'];
+class UserPersistend extends User{
+    dbId: string;
+}
 
-const roles = ['admin', 'user', 'super-user'] as const;
-type roleTypes = typeof roles[number]
-const dfdf: roleTypes = 'user'
+function getUser(dbId: string ): UserPersistend
+function getUser(id: number): User 
+function getUser(IdOrdbId: string | number): User | UserPersistend {
+    if(typeof IdOrdbId === 'string'){
+        return new UserPersistend();
+    }else{
+        return new User;
+    }
+}
+
+type UserOrUserPersistend <T extends string | number> = T extends number ? User : UserPersistend;
+
+function getUser2<T extends string | number>(id: T): UserOrUserPersistend<T> {
+    if(typeof id === 'number'){
+        return new User() as UserOrUserPersistend<T>;
+    }else{
+        return new UserPersistend() as UserOrUserPersistend<T>;
+    }
+}
+
+const res = getUser2(1);
+const res1 = getUser2('dfsdf1')
