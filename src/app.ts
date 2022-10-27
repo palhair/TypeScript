c65316b31f6f384b58ed50b78794758ce7e1f617
palhair <palad.ru@mail.ru>
2022-10-27 12:54:21 +0400

    Декоратор класса

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ interface IUserService {
     getUserInDatabase(): number;
 }
 
+//@nullUser
+@threeUserAdvanced
 class UserServise implements IUserService {
     users: number = 1000;
     getUserInDatabase(): number {
@@ -11,16 +13,17 @@ class UserServise implements IUserService {
 
 }
 
-function nullUser(obj: IUserService): IUserService{
-    obj.users = 0;
-    return obj;
+function nullUser(target: Function) {
+    target.prototype.users = 0;
+    
 }
 
-function logUsers(obj: IUserService): IUserService{
-    console.log('Users ' + obj.users);
-    return obj;
+function threeUserAdvanced <T extends { new(...args: any[]):{}} >(constructor: T) {
+    return class extends constructor {
+        users = 3;
+    }
 }
+
+
 
 console.log(new UserServise().getUserInDatabase());
-console.log(logUsers(nullUser(new UserServise())).getUserInDatabase());
-console.log(nullUser(logUsers(new UserServise())).getUserInDatabase());
