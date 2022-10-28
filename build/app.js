c5c1f018bcd87f79709a4bfcc02e0203a07854b1
palhair <palad.ru@mail.ru>
2022-10-28 14:42:19 +0400

    декоратор свойств

--- build/app.js
+++ build/app.js
@@ -5,15 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 class UserServise {
     constructor() {
         this.users = 1000;
@@ -23,26 +14,29 @@ class UserServise {
     }
 }
 __decorate([
-    Cathes(true)
-], UserServise.prototype, "getUserInDatabase", null);
-new UserServise().getUserInDatabase();
-function Cathes(rethrow = false) {
-    return (target, _, descriptor) => {
-        const oldValue = descriptor.value;
-        descriptor.value = (...args) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                return yield (oldValue === null || oldValue === void 0 ? void 0 : oldValue.apply(target, args));
+    Max(100)
+], UserServise.prototype, "users", void 0);
+const user = new UserServise();
+user.users = 1;
+console.log(user.users);
+user.users = 101;
+function Max(max) {
+    return (target, propertyKey) => {
+        let value;
+        const setter = function (newValue) {
+            if (newValue > max) {
+                console.log(`Больше ${max}`);
             }
-            catch (e) {
-                if (rethrow) {
-                    throw e;
-                }
-                else {
-                    if (e instanceof Error) {
-                        console.log(e.message);
-                    }
-                }
+            else {
+                value = newValue;
             }
+        };
+        const getter = function () {
+            return value;
+        };
+        Object.defineProperty(target, propertyKey, {
+            set: setter,
+            get: getter
         });
     };
 }
--- src/app.ts
+++ src/app.ts
@@ -4,45 +4,40 @@ interface IUserService {
 }
 
 class UserServise implements IUserService {
+    @Max(100)
     users: number = 1000 ;
 
-    @Cathes(true)
+    
     getUserInDatabase(): number {
         throw new Error('Ошибка');
     }
 
 }
 
+const user = new UserServise()
+user.users = 1;
+console.log(user.users);
+user.users = 101;
 
-new UserServise().getUserInDatabase()
-
-function Cathes(rethrow: boolean = false) {
+function Max(max: number) {
     return (
         target: Object,
-        _: string | symbol,
-        descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
-    ):TypedPropertyDescriptor<(...args: any[]) => any> | void =>
-    {
-        const oldValue = descriptor.value;
-        descriptor.value = async (...args: any[]) =>{
-           
-                try{
-                    return await oldValue?.apply(target, args);
-                    
-                }catch(e){
-                    if (rethrow) {
-                       
-                        throw e;
-                       
-                    }else{
-                        if(e instanceof Error){
-                            console.log(e.message)
-                        }
-                    }
-                }
-            
+        propertyKey: string | symbol,
+    ) => {
+        let value: number;
+        const setter = function(newValue: number){
+            if(newValue > max){
+                console.log(`Больше ${max}`)
+            }else{
+                value = newValue;
+            }
         }
-        
-    
+        const getter = function(){
+            return value;
+        }
+        Object.defineProperty(target, propertyKey, {
+            set: setter,
+            get: getter
+        })
     }
 }
