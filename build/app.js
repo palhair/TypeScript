bbf53f18dd1e70bf25ceba9754d5299e246884ac
palhair <palad.ru@mail.ru>
2022-11-15 16:55:26 +0400

    Поведенчиские паттерны - Iterator

--- build/app.js
+++ build/app.js
@@ -1,36 +1,104 @@
 "use strict";
-class User {
-}
-class Auth {
-    constructor(strategy) {
-        this.strategy = strategy;
-    }
-    setStrategy(strategy) {
-        this.strategy = strategy;
-    }
-    authUser(user) {
-        return this.strategy.auth(user);
+var _a, _b;
+class Task {
+    constructor(priority) {
+        this.priority = priority;
     }
 }
-class JWTStrategy {
-    auth(user) {
-        if (user.jwtToken) {
-            return true;
+var IteratorName;
+(function (IteratorName) {
+    IteratorName[IteratorName["INC"] = 0] = "INC";
+    IteratorName[IteratorName["DEC"] = 1] = "DEC";
+})(IteratorName || (IteratorName = {}));
+class TaskList {
+    constructor() {
+        this.tasks = [];
+    }
+    addTask(task) {
+        this.tasks.push(task);
+    }
+    sortByPriority() {
+        this.tasks.sort((a, b) => a.priority - b.priority);
+    }
+    sortDescPriority() {
+        this.tasks.sort((a, b) => b.priority - a.priority);
+    }
+    getTask() {
+        return this.tasks;
+    }
+    count() {
+        return this.tasks.length;
+    }
+    getIterator(iteratorName) {
+        let iterator;
+        switch (iteratorName) {
+            case IteratorName.INC:
+                iterator = new PriorityTaskIterator(this);
+                break;
+            case IteratorName.DEC:
+                iterator = new DescPriorityIterator(this);
+                break;
         }
-        return false;
+        return iterator;
     }
 }
-class GithubStrategy {
-    auth(user) {
-        if (user.githubToken) {
-            return true;
-        }
-        return false;
+class PriorityTaskIterator {
+    constructor(taskList) {
+        this.position = 0;
+        taskList.sortByPriority();
+        this.taskList = taskList;
+    }
+    curent() {
+        return this.taskList.getTask()[this.position];
+    }
+    next() {
+        this.position += 1;
+        return this.taskList.getTask()[this.position];
+    }
+    previos() {
+        this.position -= 1;
+        return this.taskList.getTask()[this.position];
+    }
+    index() {
+        return this.position;
     }
 }
-const user = new User();
-user.jwtToken = 'token';
-const auth = new Auth(new JWTStrategy());
-console.log(auth.authUser(user));
-auth.setStrategy(new GithubStrategy());
-console.log(auth.authUser(user));
+class DescPriorityIterator {
+    constructor(taskList) {
+        this.position = 0;
+        taskList.sortDescPriority();
+        this.taskList = taskList;
+    }
+    curent() {
+        return this.taskList.getTask()[this.position];
+    }
+    next() {
+        this.position += 1;
+        return this.taskList.getTask()[this.position];
+    }
+    previos() {
+        this.position -= 1;
+        return this.taskList.getTask()[this.position];
+    }
+    index() {
+        return this.position;
+    }
+}
+const taskList = new TaskList();
+taskList.addTask(new Task(1));
+taskList.addTask(new Task(4));
+taskList.addTask(new Task(7));
+taskList.addTask(new Task(3));
+taskList.addTask(new Task(19));
+taskList.addTask(new Task(45));
+taskList.addTask(new Task(22));
+const iterator = taskList.getIterator(IteratorName.DEC);
+while (iterator.curent() !== undefined) {
+    console.log((_a = iterator.curent()) === null || _a === void 0 ? void 0 : _a.priority);
+    iterator.next();
+}
+const deskIterator = taskList.getIterator(IteratorName.INC);
+while (deskIterator.curent() !== undefined) {
+    console.log((_b = deskIterator.curent()) === null || _b === void 0 ? void 0 : _b.priority);
+    deskIterator.next();
+}
--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,126 @@
-class User {
-    githubToken: string;
-    jwtToken: string;
+class Task {
+    constructor(public priority: number) {}
 }
 
-interface IAuthStrategy {
-    auth(user: User): boolean;
+enum IteratorName {
+    INC,
+    DEC
 }
+class TaskList {
+    private tasks: Task[] = [];
 
-class Auth {
-    constructor(private strategy: IAuthStrategy) { }
-
-    setStrategy(strategy: IAuthStrategy){
-        this.strategy = strategy;
+    public addTask(task: Task){
+        this.tasks.push(task);
     }
 
-    public authUser(user: User): boolean {
-        return this.strategy.auth(user);
+    public sortByPriority(){
+        this.tasks.sort((a, b) => a.priority - b.priority)
     }
-}
+    public sortDescPriority(){
+        this.tasks.sort((a, b) => b.priority - a.priority)
+    }
 
-class JWTStrategy implements IAuthStrategy {
-    auth(user: User): boolean {
-        if(user.jwtToken){
-            return true;
+    public getTask(){
+        return this.tasks;
+    }
+
+    public count(){
+        return this.tasks.length;
+    }
+
+    public getIterator(iteratorName: IteratorName){
+        let iterator;
+        switch(iteratorName){
+            case IteratorName.INC:
+                iterator = new PriorityTaskIterator(this);
+                break;
+            case IteratorName.DEC:
+                iterator = new DescPriorityIterator(this);
+                break;
+
         }
-        return false;
-    }
-
-}
-class GithubStrategy implements IAuthStrategy {
-    auth(user: User): boolean {
-        if(user.githubToken){
-            return true;
-        }
-        return false;
+        return iterator;
     }
 
 }
 
-const user = new User();
-user.jwtToken = 'token';
+interface IIterator<T> {
+    curent(): T | undefined;
+    next(): T | undefined;
+    previos(): T | undefined;
+    index(): number;
+}
 
-const auth = new Auth(new JWTStrategy());
-console.log(auth.authUser(user));
-auth.setStrategy(new GithubStrategy());
-console.log(auth.authUser(user));
+class PriorityTaskIterator implements IIterator<Task> {
+    private position: number = 0;
+    private taskList: TaskList;
+
+    constructor(taskList: TaskList){
+        taskList.sortByPriority();
+        this.taskList = taskList;
+    }
+
+    curent(): Task | undefined {
+        return this.taskList.getTask()[this.position];
+    }
+    next(): Task | undefined {
+        this.position += 1;
+        return this.taskList.getTask()[this.position];
+    }
+    previos(): Task | undefined {
+        this.position -= 1;
+        return this.taskList.getTask()[this.position];
+    }
+    index(): number {
+        return this.position;
+    }
+
+}
+class DescPriorityIterator implements IIterator<Task> {
+    private position: number = 0;
+    private taskList: TaskList;
+
+    constructor(taskList: TaskList){
+        taskList.sortDescPriority();
+        this.taskList = taskList;
+    }
+
+    curent(): Task | undefined {
+        return this.taskList.getTask()[this.position];
+    }
+    next(): Task | undefined {
+        this.position += 1;
+        return this.taskList.getTask()[this.position];
+    }
+    previos(): Task | undefined {
+        this.position -= 1;
+        return this.taskList.getTask()[this.position];
+    }
+    index(): number {
+        return this.position;
+    }
+
+}
+
+const taskList = new TaskList();
+taskList.addTask(new Task(1));
+taskList.addTask(new Task(4));
+taskList.addTask(new Task(7));
+taskList.addTask(new Task(3));
+taskList.addTask(new Task(19));
+taskList.addTask(new Task(45));
+taskList.addTask(new Task(22));
+
+const iterator = taskList.getIterator(IteratorName.DEC);
+
+while(iterator.curent() !== undefined ){
+    console.log(iterator.curent()?.priority);
+    iterator.next();
+}
+
+const deskIterator = taskList.getIterator(IteratorName.INC);
+
+while(deskIterator.curent() !== undefined ){
+    console.log(deskIterator.curent()?.priority);
+    deskIterator.next();
+}
